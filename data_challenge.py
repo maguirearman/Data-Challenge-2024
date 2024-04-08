0b3f0873941b3220342c9117e2dfcd14462332ac
maguirearman <arman.29@osu.edu>
2024-04-08 16:16:29 -0400

    added comments and improved score to 0.795

--- data_challenge.py
+++ data_challenge.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.pipeline import Pipeline
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.metrics import roc_auc_score
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import RandomForestClassifier
+# from xgboost import XGBClassifier
+# from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+# from sklearn.pipeline import Pipeline
 
+# 2024 Data Challenge (CSE 5243)
 
 # Load the training data
 train_demos_df = pd.read_csv("train/train_demos.csv")
@@ -18,14 +19,10 @@ train_signs_df = pd.read_csv("train/train_signs.csv")
 train_radiology_df = pd.read_csv("train/train_radiology.csv")
 train_labels_df = pd.read_csv("train/train_labels.csv")
 
-#Load the testing data
-test_demos_df = pd.read_csv("test/test_demos.csv")
-test_signs_df = pd.read_csv("test/test_signs.csv")
-test_radiology_df = pd.read_csv("test/test_radiology.csv")
+# Feature Engineering
+# Start by preprocessing vitals data (train_signs_df)
 
-# Preprocessing vitals data
-
-# Define thresholds
+# Define thresholds (Used internet to get the thresholds for each vital measure)
 healthy_hr_min, healthy_hr_max = 60, 100
 healthy_resp_min, healthy_resp_max = 12, 25
 healthy_nbp_sys_min, healthy_nbp_sys_max = 90, 140
@@ -47,7 +44,7 @@ phosphate_normal_min, phosphate_normal_max = 2.5, 4.5
 platelet_normal_min, platelet_normal_max = 150, 450
 potassium_normal_min, potassium_normal_max = 3.0, 6.0
 
-# Process training data to calculate flags based on thresholds
+# Process training data to calculate flags based on thresholds (use group by to get info for each patient and check all tests for each patient)
 flags_df = train_signs_df.groupby('patient_id').agg({
     'heartrate': lambda hr: ((hr < healthy_hr_min) | (hr > healthy_hr_max)).any().astype(int),
     'resp': lambda r: ((r < healthy_resp_min) | (r > healthy_resp_max)).any().astype(int),
@@ -72,45 +69,12 @@ flags_df = train_signs_df.groupby('patient_id').agg({
 
 }).reset_index()
 
-# Merge flags with labels
+# Merge flags with labels to start building feature vector
 features_df = pd.merge(flags_df, train_labels_df, on='patient_id', how='left')
 
-# Calculate counts for each vital measure
-vital_counts_df = train_signs_df.groupby('patient_id').agg({
-    'heartrate': 'count',
-    'resp': 'count',
-    'nbpsys': 'count',
-    'spo2': 'count',
-    'nbpdia': 'count',
-    'nbpmean': 'count',
-    'aniongap': 'count',
-    'bicarbonate': 'count',
-    'chloride': 'count',
-    'creatinine': 'count',
-    'calcium': 'count',
-    'glucose': 'count',
-    'hematocrit': 'count',
-    'hemoglobin': 'count',
-    'mch': 'count',
-    'mcv': 'count',
-    'magnesium': 'count',
-    'phosphate': 'count',
-    'platelet': 'count',
-    'potassium': 'count'
-}).reset_index()
+# Preprocessing demographic data (train_demos_df)
 
-# Sum the counts to get a total measure count per patient
-vital_counts_df['total_vital_measures'] = vital_counts_df.drop('patient_id', axis=1).sum(axis=1)
-
-# Keep only patient_id and total_vital_measures for merging
-vital_measures_total_df = vital_counts_df[['patient_id', 'total_vital_measures']]
-
-# Merge with the features DataFrame
-features_df = pd.merge(features_df, vital_measures_total_df, on='patient_id', how='left')
-
-# Preprocessing demographic data
-
-# Convert 'admittime' to datetime
+# Convert 'admittime' to datetime (continuous variable that represnts time)
 train_demos_df['admittime'] = pd.to_datetime(train_demos_df['admittime'])
 
 # Extract useful features from 'admittime' (e.g., hour of admission, month)
@@ -123,25 +87,31 @@ train_demos_df['gender'] = train_demos_df['gender'].map({'M': 1, 'F': 0})
 # One-hot encode categorical variables ('insurance', 'marital_status', 'ethnicity')
 train_demos_df = pd.get_dummies(train_demos_df, columns=['insurance', 'marital_status', 'ethnicity'], drop_first=True)
 
+# Merge the demographic data with the features DataFrame
+features_df = pd.merge(features_df, train_demos_df.drop(['admittime'], axis=1), on='patient_id', how='left')
+
+
 # Pre process radiology data
-# Done in radio.py
+
+# The feature generation process is done in radio.py (due to how long it takes to run)
 # Load the radiology feature vector CSV
 radiology_features_df = pd.read_csv('radiology_feature_vector.csv')
 
-# Set k to the number of features to keep
+# Set k to the number of features to keep (defined in radio.py)
+# k=1000 gave us the best performance
 k=1000
-# Merge radio data vector as well
 
+# Merge radio data vector with the features DataFrame
 features_df = pd.merge(features_df, radiology_features_df, on='patient_id', how='left')
-radiology_feature_columns = [str(i) for i in range(0, k)]  # Adjust this range based on your feature columns
 
-features_df = pd.merge(features_df, train_demos_df.drop(['admittime'], axis=1), on='patient_id', how='left')
+radiology_feature_columns = [str(i) for i in range(0, k)]  
 
 # Prepare features and labels for modeling
-X_columns = ['heartrate', 'resp', 'nbpsys', 'spo2', 'nbpdia', 'nbpmean', 'aniongap', 'bicarbonate', 'chloride', 'creatinine', 'calcium', 'glucose', 'hematocrit', 'hemoglobin', 'mch', 'magnesium', 'phosphate', 'platelet', 'potassium', 'age', 'gender', 'admit_hour', 'admit_month', 'total_vital_measures'] + \
+X_columns = ['heartrate', 'resp', 'nbpsys', 'spo2', 'nbpdia', 'nbpmean', 'aniongap', 'bicarbonate', 'chloride', 'creatinine', 'calcium', 'glucose', 'hematocrit', 'hemoglobin', 'mch', 'magnesium', 'phosphate', 'platelet', 'potassium', 'age', 'gender', 'admit_hour', 'admit_month'] + \
             [col for col in features_df.columns if col.startswith('insurance_') or col.startswith('marital_status_') or col.startswith('ethnicity_')]  + \
             radiology_feature_columns
 
+# set X and Y to be the features and the Labels
 X = features_df[X_columns].to_numpy()
 y = features_df['label'].to_numpy().astype(int)
 
@@ -153,43 +123,43 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_val_scaled = scaler.transform(X_val)
 
-# Logistic Regression Model
-log_reg_model = LogisticRegression(max_iter=1000) # Increased max_iter for convergence
+# # Logistic Regression Model
+# log_reg_model = LogisticRegression(max_iter=1000) # Increased max_iter for convergence
 
-# Train the model
-log_reg_model.fit(X_train_scaled, y_train)
+# # Train the model
+# log_reg_model.fit(X_train_scaled, y_train)
 
-# Predict and evaluate
-y_val_pred_proba = log_reg_model.predict_proba(X_val_scaled)[:, 1]
-val_auroc_score = roc_auc_score(y_val, y_val_pred_proba)
-print(f"Logistic Regression Validation AUROC Score: {val_auroc_score:.4f}")
+# # Predict and evaluate
+# y_val_pred_proba = log_reg_model.predict_proba(X_val_scaled)[:, 1]
+# val_auroc_score = roc_auc_score(y_val, y_val_pred_proba)
+# print(f"Logistic Regression Validation AUROC Score: {val_auroc_score:.4f}")
 
 
-# Prepare a pipeline for scaling and Logistic Regression
-pipeline = Pipeline([
-    ('scaler', StandardScaler()),
-    ('log_reg', LogisticRegression())
-])
+# # Prepare a pipeline for scaling and Logistic Regression
+# pipeline = Pipeline([
+#     ('scaler', StandardScaler()),
+#     ('log_reg', LogisticRegression())
+# ])
 
-# Parameters grid for Logistic Regression
-param_grid = {
-    'log_reg__C': np.logspace(-4, 4, 20),
-    'log_reg__solver': ['liblinear']  # 'liblinear' is a good choice for small datasets and binary classification
-}
+# # Parameters grid for Logistic Regression
+# param_grid = {
+#     'log_reg__C': np.logspace(-4, 4, 20),
+#     'log_reg__solver': ['liblinear']  # 'liblinear' is a good choice for small datasets and binary classification
+# }
 
-# Setup GridSearchCV
-grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='roc_auc')
-grid_search.fit(X_train, y_train)
+# # Setup GridSearchCV
+# grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='roc_auc')
+# grid_search.fit(X_train, y_train)
 
-# Evaluate the best model from grid search
-best_model = grid_search.best_estimator_
-y_val_pred_proba = best_model.predict_proba(X_val)[:, 1]
-val_auroc_score = roc_auc_score(y_val, y_val_pred_proba)
-print(f"Logistic Regression Best Model (with GridSearch) Validation AUROC Score: {val_auroc_score:.4f}")
+# # Evaluate the best model from grid search
+# best_model = grid_search.best_estimator_
+# y_val_pred_proba = best_model.predict_proba(X_val)[:, 1]
+# val_auroc_score = roc_auc_score(y_val, y_val_pred_proba)
+# print(f"Logistic Regression Best Model (with GridSearch) Validation AUROC Score: {val_auroc_score:.4f}")
 
 # Define other models for comparison
 models = {
-    'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42),
+    # 'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42),
     'GBM (Gradient Boosting)': GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
 }
 
@@ -199,3 +169,13 @@ for name, model in models.items():
     y_val_pred_proba = model.predict_proba(X_val_scaled)[:, 1]  # Probability estimates
     val_auroc_score = roc_auc_score(y_val, y_val_pred_proba)
     print(f"{name} Validation AUROC Score: {val_auroc_score:.4f}")
+
+#Load the testing data
+test_demos_df = pd.read_csv("test/test_demos.csv")
+test_signs_df = pd.read_csv("test/test_signs.csv")
+test_radiology_df = pd.read_csv("test/test_radiology.csv")
+
+# Use test data to predict mortality
+
+
+# generate output csv (CSV file with `patient_id, probability` as headers, each line for patient ID and the probability of the patient's in-hospital mortality.)
